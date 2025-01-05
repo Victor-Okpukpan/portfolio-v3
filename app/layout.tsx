2730b9952682959ba01cafb00aff871248790a54
Victor Okpukpan <vokpukpan@gmail.com>
2025-01-05 22:27:15 +0100

    chore: update metadata for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,71 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-import AnimatedBackground from '@/components/animated-background'
-import LoadingScreen from '@/components/LoadingScreen'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import AnimatedBackground from "@/components/animated-background";
+import LoadingScreen from "@/components/LoadingScreen";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Victor Okpukpan - Solidity Smart Contract Developer | Frontend Engineer',
-  description: 'Portfolio of Victor Okpukpan, a Solidity Smart Contract Developer and Frontend Engineer specializing in EVM-compatible blockchain development',
-  keywords: 'Solidity, Smart Contracts, Blockchain Developer, Frontend Engineer, Web3, EVM, DApp Development, React, Next.js, Ethereum, Portfolio',
-  author: 'Victor Okpukpan',
-  metadataBase: new URL('https://victortheoracle.xyz'),
+  title: "Victor Okpukpan | Solidity Developer & Frontend Engineer",
+  description:
+    "Victor Okpukpan, a Solidity Smart Contract Developer, security researcher and Frontend Engineer specializing in EVM-compatible blockchain development.",
+  keywords: [
+    "Victor Okpukpan",
+    "Victortheoracle",
+    "Victor the oracle",
+    "Solidity Developer",
+    "Smart Contract developer",
+    "Blockchain Developer",
+    "Frontend Engineer",
+    "Frontend developer",
+    "Web3 developer",
+    "EVM developer",
+    "DApp Developer",
+    "React developer",
+    "Next.js Developer",
+    "Security researcher",
+    "Ethereum developer",
+    "Portfolio",
+  ],
+  author: "Victor Okpukpan",
+  metadataBase: new URL("https://victortheoracle.xyz"),
   openGraph: {
-    title: 'Victor Okpukpan - Solidity Smart Contract Developer | Frontend Engineer',
-    description: 'Portfolio of Victor Okpukpan, a Solidity Smart Contract Developer and Frontend Engineer specializing in EVM-compatible blockchain development',
-    type: 'website',
-    locale: 'en_US',
-    url: 'https://victortheoracle.xyz',
-    siteName: 'Victor Okpukpan Portfolio',
+    title: "Victor Okpukpan | Solidity Developer & Frontend Engineer",
+    description:
+      "Victor Okpukpan, a Solidity Smart Contract Developer, security researcher and Frontend Engineer specializing in EVM-compatible blockchain development.",
+    type: "website",
+    locale: "en_US",
+    url: "https://victortheoracle.xyz",
+    siteName: "Victor Okpukpan Portfolio",
     images: [
       {
-        url: '/pfp.jpg',
+        url: "/pfp.jpg",
         width: 300,
         height: 300,
-        alt: 'Victor Okpukpan',
-      }
+        alt: "Victor Okpukpan",
+      },
     ],
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Victor Okpukpan - Solidity Smart Contract Developer | Frontend Engineer',
-    description: 'Portfolio of Victor Okpukpan, a Solidity Smart Contract Developer and Frontend Engineer specializing in EVM-compatible blockchain development',
-    images: ['/pfp.jpg'],
+    card: "summary_large_image",
+    title: "Victor Okpukpan | Solidity Developer & Frontend Engineer",
+    description:
+      "Victor Okpukpan, a Solidity Smart Contract Developer, security researcher and Frontend Engineer specializing in EVM-compatible blockchain development.",
+    images: ["/pfp.jpg"],
   },
   robots: {
     index: true,
     follow: true,
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -61,6 +81,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
-
